7d6a8d5a47d06a15ead1d8ff8f0dc3ae06531ba4
root <root@ip-172-31-32-112.ec2.internal>
2024-02-05 05:21:06 +0000

    my first commit to git

---
+++ java-app/src/main/java/com/app/example/App.java
@@ -0,0 +1,15 @@
+package com.app.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "This is priyas first pipeline deployment!" );
+
+    }
+}
